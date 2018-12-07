8454b4e014a058567a10303612ba4c3b54833422
Ares <ga8291900@gmail.com>
2018-12-08 02:00:18 +0800

    分頁標籤完成

--- js/all.js
+++ js/all.js
@@ -1,6 +1,7 @@
 (function () {
 
   const items_option = document.querySelector('#items_option')
+  let x = 1
   // 初始化下拉式選單
   function createSelect () {
     const xhr = new XMLHttpRequest();
@@ -40,24 +41,24 @@
         document.querySelector('.items_title').textContent = SelectName;
       }
       for(i=0; i<pageNum; i++){
-        str += `<li class="page-item"><a class="page-link" href="#">${i+1}</a></li>`;
+        str += `<li class="page-item"><a class="page-link item_pages" href="#">${i+1}</a></li>`;
       }
       if ( pageNum == 1 ){
         item_page.innerHTML = str;
       } else {
       item_page.innerHTML = 
       `<li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-          </a>
-        </li>` + str +
-        `<li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-          </a>
-        </li>`;
+        <a class="page-link item_pages_pre" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+          <span class="sr-only">Previous</span>
+        </a>
+      </li>` + str +
+      `<li class="page-item">
+        <a class="page-link item_pages_next" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+          <span class="sr-only">Next</span>
+        </a>
+      </li>`;
       }
       createCard (datas)
     };
@@ -65,8 +66,8 @@
 
   // 初始化內容卡片
   function createCard (datas) {
-    console.log(datas)
     let str = '';
+    let pageNum = Math.ceil(datas.length / 10);
     datas.forEach(data => {
       str += `<div class="col-sm-6 mb-5">
         <div class="card shadow">
@@ -98,15 +99,52 @@
         </div>
       </div>`;
     });
-    items_content.innerHTML = str
+    document.querySelector('#items_content').innerHTML = str
+    changepage(datas, 1);
+
+
+    let pages = document.querySelectorAll('.item_pages');
+    pages.forEach(page => {
+      page.addEventListener('click', function() {
+      x = page.textContent
+        changepage( datas, x)
+      })
+    });
+
+    document.querySelector('.item_pages_pre').addEventListener('click', function () {
+      x = parseInt(x) - 1
+      if ( x < 1) { x = 1};
+      changepage( datas, x)
+    })
+
+    document.querySelector('.item_pages_next').addEventListener('click', function () {
+      x = parseInt(x) + 1
+      if ( x > pageNum) {x = pageNum}
+      changepage( datas, x)
+    })
   }
 
+  function changepage ( datas, x) {
+    let cuttentpage = x
+    let pageNum = Math.ceil(datas.length / 10);
+    let endpage = 10 + (cuttentpage-1)*10 ;
+    if ( endpage > datas.length){endpage = datas.length};
 
+    document.querySelectorAll('#items_content > div')
+    .forEach( item => item.style.display = 'none');
 
+    for (let i = (cuttentpage-1)*10; i < endpage; i++) {
+      document.querySelectorAll('#items_content > div')[i].style.display = 'block';
+    }
+    document.querySelectorAll('#item_page li').forEach(element => {
+      element.classList.remove('active')
+    });
+    document.querySelectorAll('#item_page li')[x].classList.add('active')
+  }
 
-
+  createSelect ();
+  createPagination ();
   items_option.addEventListener('change', createPagination, false)
 
-  createSelect();
-  createPagination ();
+  
 }());
