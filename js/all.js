0c8bd4e80587ed3b326fe32125a554656d29e9e1
Ares <ga8291900@gmail.com>
2018-12-07 13:44:49 +0800

    完成分頁切版與Card

--- js/all.js
+++ js/all.js
@@ -1,30 +1,30 @@
 (function () {
 
-  // 新增下拉式選單
+  const items_option = document.querySelector('#items_option')
+  // 初始化下拉式選單
   function createSelect () {
     const xhr = new XMLHttpRequest();
     xhr.open('get','https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97&limit=999', true);
     xhr.send(null);
 
     xhr.onload = function () {
-      const data = JSON.parse(xhr.response).result.records;
-      let areaName = data.map(item => item.Zone);
-      let areaList = new Set(['所有地區', ...areaName]);
+      const datas = JSON.parse(xhr.response).result.records;
+      let areaName = datas.map(item => item.Zone);
+      let areaList = new Set(['所有區域', ...areaName]);
       areaList.forEach(element => {
         const option = document.createElement('option');
         option.setAttribute('value', element);
         option.appendChild(document.createTextNode(element));
-        document.querySelector('#items_option').appendChild(option);
+        items_option.appendChild(option);
       });
     };
   };
-  createSelect();
 
-  //取得各區資料
-  function createCard () {
+  // 初始化分頁列表
+  function createPagination () {
     const xhr = new XMLHttpRequest();
-    let SelectName = document.querySelector('#items_option').value;
-    if( SelectName == '所有區域') {
+    let SelectName = items_option.value;
+    if( !SelectName ) {
     xhr.open('get','https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97&limit=999', true);
     } else {
       xhr.open('get',`https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97&q=${SelectName}`, true);
@@ -32,9 +32,78 @@
     xhr.send(null);
 
     xhr.onload = function () {
-      // const data = JSON.parse(xhr.response).result.records;
-      // console.log()
+      let datas = JSON.parse(xhr.responseText).result.records;
+      let pageNum = Math.ceil(datas.length / 10);
+      const item_page = document.querySelector('#item_page');
+      let str = '';
+      for(i=0; i<pageNum; i++){
+        str += `<li class="page-item"><a class="page-link" href="#">${i+1}</a></li>`;
+      }
+      if ( pageNum == 1 ){
+        item_page.innerHTML = str;
+      } else {
+      item_page.innerHTML = 
+      `<li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>` + str +
+        `<li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>`;
+      }
+      createCard (datas)
     };
   }
-    document.querySelector('#items_option').addEventListener('change', createCard, false)
+
+  // 初始化內容卡片
+  function createCard (datas) {
+    console.log(datas)
+    let str = '';
+    datas.forEach(data => {
+      str += `<div class="col-sm-6 mb-5">
+        <div class="card shadow">
+          <div class="card-header d-flex align-items-end justify-content-between bg-item" 
+          style="background-image: url(${data.Picture1})">
+            <h5 class="text-white mb-0">${data.Name}</h5>
+            <h6 class="text-white mb-0">${data.Zone}</h6>
+          </div>
+          <div class="card-body">
+            <ul class="list-unstyled mb-0">
+              <li>
+                <i class="fas fa-clock"></i>
+                <span>${data.Opentime}</span>
+              </li>
+              <li>
+                <i class="fas fa-map-marker-alt"></i>
+                <span>${data.Add}</span>
+              </li>
+              <li>
+                <i class="fas fa-phone"></i>
+                <span>${data.Tel}</span>
+              </li>
+              <li class="text-info">
+                <i class="fas fa-tag"></i>
+                <span>${data.Ticketinfo}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>`;
+    });
+    items_content.innerHTML = str
+  }
+
+
+
+
+
+  items_option.addEventListener('change', createPagination, false)
+
+  createSelect();
+  createPagination ();
 }());
